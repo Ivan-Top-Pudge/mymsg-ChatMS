04656f04c60002e5bec901ba85255b67aee042e1
Vano <vananufr1ev@yandex.ru>
2026-06-18 11:37:02 +0300

    storage level done

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -61,12 +61,38 @@ func (s *Storage) CreateChat(
 	return chatID, nil
 }
 
+// DeleteChat deletes chat with given chatID
 func (s *Storage) DeleteChat(ctx context.Context, chatID int64) error {
-	panic("Imlement me")
+	const op = "storage.postgres.DeleteChat"
+
+	tag, err := s.pool.Exec(ctx, "DELETE FROM chats WHERE id = $1", chatID)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if tag.RowsAffected() == 0 {
+		return fmt.Errorf("%s: chat not found", op)
+	}
+
+	return nil
 }
 
+// ChatExists returns true if chat with chatID exists in db
 func (s *Storage) ChatExists(ctx context.Context, chatID int64) (bool, error) {
-	panic("Imlement me")
+	const op = "storage.postgres.ChatExists"
+
+	var exists bool
+
+	err := s.pool.QueryRow(
+		ctx,
+		"SELECT EXISTS(SELECT 1 FROM chats WHERE id = $1)",
+		chatID,
+	).Scan(&exists)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return exists, nil
 }
 
 func (s *Storage) SaveMessage(ctx context.Context, chatID int64, senderID int64, text string) (int64, error) {
@@ -92,9 +118,55 @@ func (s *Storage) SaveMessage(ctx context.Context, chatID int64, senderID int64,
 }
 
 func (s *Storage) DeleteMessage(ctx context.Context, msgID int64, chatID int64) error {
-	panic("Imlement me")
+	const op = "storage.postgres.DeleteMessage"
+
+	tag, err := s.pool.Exec(
+		ctx,
+		"DELETE FROM messages WHERE id = $1 AND chat_id = $2",
+		msgID,
+		chatID,
+	)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if tag.RowsAffected() == 0 {
+		return fmt.Errorf("%s: message not found", op)
+	}
+
+	return nil
 }
 
 func (s *Storage) GetHistory(ctx context.Context, chatID int64, limit int64, offset int64) ([]models.Message, error) {
-	panic("Implement me")
+	const op = "storage.postgres.GetHistory"
+
+	query := `
+	SELECT id, chat_id, sender_id, text, created_at
+	FROM messages
+	WHERE chat_id = $1
+	ORDER BY created_at DESC
+	LIMIT $2 OFFSET $3;
+	`
+	rows, err := s.pool.Query(ctx, query, chatID, limit, offset)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	defer rows.Close()
+
+	messages := make([]models.Message, 0)
+	var msg models.Message
+	for rows.Next() {
+		err := rows.Scan(&msg.ID, &msg.ChatID, &msg.SenderID, &msg.Text, &msg.CreatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("%s: failed to scan message: %w", op, err)
+		}
+		messages = append(messages, msg)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("%s: rows iteration error: %w", op, err)
+	}
+
+	return messages, nil
 }
